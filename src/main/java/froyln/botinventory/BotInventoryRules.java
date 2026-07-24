a918e7ef5eb7f2cf1f01bacbc50a5c33a0a8d1c8
froyln <froylanpd74@gmail.com>
2026-07-23 18:32:30 -0700

    Fix @Rule annotations and Carpet API usage
    - Removed invalid desc field from @Rule (not part of Carpet API)
    - Added required categories field
    - Added strict=false for custom string options
    - Kept String type for rules with op/permission level options

--- src/main/java/froyln/botinventory/BotInventoryRules.java
+++ src/main/java/froyln/botinventory/BotInventoryRules.java
@@ -2,22 +2,28 @@ package froyln.botinventory;
 
 import carpet.api.settings.Rule;
 
+import static carpet.api.settings.RuleCategory.COMMAND;
+import static carpet.api.settings.RuleCategory.EXPERIMENTAL;
+
 public class BotInventoryRules {
     @Rule(
-        desc = "Allows right click in fake player to view their inventories",
-        options = {"true", "false", "op", "0", "1", "2", "3", "4"}
+        categories = {EXPERIMENTAL, COMMAND},
+        options = {"true", "false", "op", "0", "1", "2", "3", "4"},
+        strict = false
     )
     public static String clickFakePlayerInventory = "false";
 
     @Rule(
-        desc = "Command to view a player's inventory",
-        options = {"true", "false", "op", "0", "1", "2", "3", "4"}
+        categories = {COMMAND},
+        options = {"true", "false", "op", "0", "1", "2", "3", "4"},
+        strict = false
     )
     public static String viewPlayerInventoryCommand = "false";
 
     @Rule(
-        desc = "Command to view a player's enderchest",
-        options = {"true", "false", "op", "0", "1", "2", "3", "4"}
+        categories = {COMMAND},
+        options = {"true", "false", "op", "0", "1", "2", "3", "4"},
+        strict = false
     )
     public static String viewPlayerEnderchestCommand = "false";
 }
