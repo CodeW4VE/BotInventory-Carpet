0f29166d13cd85a79559f308fa25b4e53e05875b
froyln <froylanpd74@gmail.com>
2026-07-23 17:56:30 -0700

    Add base Carpet rules for BotInventory
    - clickFakePlayerInventory
    - viewPlayerInventoryCommand
    - viewPlayerEnderchestCommand
    
    All rules with true/false/op/0-4 permission options, no implementation yet.

---
+++ src/main/java/froyln/botinventory/BotInventoryRules.java
@@ -0,0 +1,23 @@
+package froyln.botinventory;
+
+import carpet.api.rule.Rule;
+
+public class BotInventoryRules {
+    @Rule(
+        desc = "Allows clicking items in fake player inventories",
+        options = {"true", "false", "op", "0", "1", "2", "3", "4"}
+    )
+    public static String clickFakePlayerInventory = "false";
+
+    @Rule(
+        desc = "Command to view a player's inventory",
+        options = {"true", "false", "op", "0", "1", "2", "3", "4"}
+    )
+    public static String viewPlayerInventoryCommand = "false";
+
+    @Rule(
+        desc = "Command to view a player's enderchest",
+        options = {"true", "false", "op", "0", "1", "2", "3", "4"}
+    )
+    public static String viewPlayerEnderchestCommand = "false";
+}
